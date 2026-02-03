d5ce158f763dc6f2eabe84c930a11bea2baf9fe8
wise_wolftron <156741883+Ashish8668@users.noreply.github.com>
2026-02-04 00:13:07 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace with your actual Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyC2KhQflSTgUvyG3AzWZc93dk_PTXyaSYo",
-    authDomain: "nyaymitra-2.firebaseapp.com",
-    projectId: "nyaymitra-2",
-    storageBucket: "nyaymitra-2.firebasestorage.app",
-    messagingSenderId: "650393846101",
-    appId: "1:650393846101:web:ae0b69f4bd7d38b0e56fdc"
+    apiKey: "AIzaSyCffvnhgTgUvyG3AzWZc93dk_PTXyaSYo",
+    authDomain: "nyaymitra.firebaseapp.com",
+    projectId: "nyaymitra",
+    storageBucket: "nyaymitra.firebasestorage.app",
+    messagingSenderId: "650395101",
+    appId: "1:650393846101:web:ae0b69f4bd7d3856fdc"
 };
 
 const app = initializeApp(firebaseConfig);
